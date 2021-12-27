4c04ed3208d61b789d657ddd967901472cbf4596
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2021-12-27 09:21:22 -0500

    chore: :sparkles: linters

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': RULES.OFF,
+  },
+};
